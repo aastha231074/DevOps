fa86401545a6d19c4c6f2613f820f2f14320239f
Aastha <asingla1101@gmail.com>
2025-10-13 06:17:59 +0530

    flask application

---
+++ Flask/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    day_of_week = datetime.today().strftime('%A')
+    current_time = datetime.now().strftime('%H:%M:%S')
+    return render_template('index.html', day_of_week=day_of_week, current_time=current_time)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
