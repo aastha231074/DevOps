652d795069adb71bca3ba75cc38eea3a050a4106
Aastha <asingla1101@gmail.com>
2025-10-17 07:30:13 +0530

    monolithic to microservice setup

--- Flask/backend/app.py
+++ Flask/backend/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, render_template
-from datetime import datetime
+from flask import Flask, request
 from dotenv import load_dotenv
 import os 
 from pymongo.mongo_client import MongoClient
@@ -13,12 +12,6 @@ db = client.test
 collection = db['flask-tutorial']
 
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-    day_of_week = datetime.today().strftime('%A')
-    current_time = datetime.now().strftime('%H:%M:%S')
-    return render_template('index.html', day_of_week=day_of_week, current_time=current_time)
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -34,11 +27,13 @@ def submit():
 
 @app.route('/view')
 def view():
-    entries = list(collection.find({}, {'_id': 0}))
-    return render_template('view.html', entries=entries)
+    data = list(collection.find())
+    for item in data:
+        print(item)
+        del item['_id']
+    return {'data': data}
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=9000,debug=True)
 
-# python -m pip install "pymongo[srv]==3.11"
---
+++ Flask/frontend/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+from dotenv import load_dotenv
+import requests
+
+BACKEND_URL = "http://127.0.0.1:9000"
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    day_of_week = datetime.today().strftime('%A')
+    current_time = datetime.now().strftime('%H:%M:%S')
+    return render_template('index.html', day_of_week=day_of_week, current_time=current_time)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+   form_data = dict(request.form)
+   response = requests.post(f"{BACKEND_URL}/submit", data=form_data)
+   return response.text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8000,debug=True)
